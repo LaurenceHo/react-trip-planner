30db6ea291a718d456236700ac589c2edf330f1e
Laurence Ho <bluegray.he@aol.com>
2019-05-27 16:51:08 +1200

    Initial commit

---
+++ client/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import 'whatwg-fetch';
+import './style/style.css';
+import { App } from './pages/app';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ client/src/services/trip-service.ts
@@ -0,0 +1,92 @@
+import { ApiService } from './api-service';
+import { Event } from '../models/event';
+import { Trip } from '../models/trip';
+import { TripDay } from '../models/trip-day';
+
+const SERVER_URL = 'http://localhost:3000/api';
+
+export class TripService {
+  apiService = new ApiService();
+
+  getTripList(requestBody: any): any {
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform('POST', `${SERVER_URL}/trip`, requestBody, searchParams, formParams);
+  }
+
+  getTripDetailWithDays(tripId: number): any {
+    const requestBody = {};
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform('GET', `${SERVER_URL}/trip/${tripId}`, requestBody, searchParams, formParams);
+  }
+
+  getTripDayWithEvents(tripId: number, tripDayId: number): any {
+    const requestBody = {};
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform(
+      'GET',
+      `${SERVER_URL}/trip/${tripId}/day/${tripDayId}`,
+      requestBody,
+      searchParams,
+      formParams
+    );
+  }
+
+  createTrip(requestBody: Trip): any {
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform('POST', `${SERVER_URL}/trip/create`, requestBody, searchParams, formParams);
+  }
+
+  createTripDay(requestBody: TripDay): any {
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform(
+      'POST',
+      `${SERVER_URL}/trip/${requestBody.trip_id}/day/create`,
+      requestBody,
+      searchParams,
+      formParams
+    );
+  }
+
+  createTripEvent(tripId: number, requestBody: Event): any {
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform(
+      'POST',
+      `${SERVER_URL}/trip/${tripId}/day/${requestBody.trip_day_id}/event/create`,
+      requestBody,
+      searchParams,
+      formParams
+    );
+  }
+
+  updateTrip(requestBody: Trip): any {
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform('PUT', `${SERVER_URL}/trip/update`, requestBody, searchParams, formParams);
+  }
+
+  updateTripEvent(tripId: number, requestBody: Event): any {
+    const searchParams = {};
+    const formParams = {};
+
+    return this.apiService.perform(
+      'PUT',
+      `${SERVER_URL}/trip/${tripId}/day/${requestBody.trip_day_id}/event/update`,
+      requestBody,
+      searchParams,
+      formParams
+    );
+  }
+}
