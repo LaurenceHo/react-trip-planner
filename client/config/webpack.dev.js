87c78117bb376948e26365f3908c8b17827f62ad
Laurence Ho <bluegray.he@aol.com>
2020-12-05 11:07:55 +1300

    Update npm modules and webpack config files

--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 const common = require('./webpack.common.js');
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     historyApiFallback: true,
     hot: true,
     inline: true,
-    port: 8080,
+    open: true,
   },
   plugins: [
     new DefinePlugin({
@@ -19,13 +19,6 @@ module.exports = merge(common, {
         NODE_ENV: JSON.stringify('development'),
       },
     }),
-    /*
-     * Plugin: MiniCssExtractPlugin
-     * Description: This plugin extracts CSS into separate files.
-     * It creates a CSS file per JS file which contains CSS.
-     * It supports On-Demand-Loading of CSS and SourceMaps.
-     * See: https://github.com/webpack-contrib/mini-css-extract-plugin
-     */
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
--- client/config/webpack.prod.js
+++ client/config/webpack.prod.js
@@ -1,9 +1,10 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 const common = require('./webpack.common.js');
+const IgnorePlugin = require('webpack/lib/IgnorePlugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -14,9 +15,16 @@ module.exports = merge(common, {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
+    /*
+     * Plugin: MiniCssExtractPlugin
+     * Description: This plugin extracts CSS into separate files.
+     * It creates a CSS file per JS file which contains CSS.
+     * It supports On-Demand-Loading of CSS and SourceMaps.
+     * See: https://github.com/webpack-contrib/mini-css-extract-plugin
+     */
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
+      filename: '[name].[fullhash].css',
+      chunkFilename: '[id].[fullhash].css',
     }),
     /*
      * Plugin: CleanWebpackPlugin
@@ -24,6 +32,7 @@ module.exports = merge(common, {
      * See: https://github.com/johnagan/clean-webpack-plugin
      */
     new CleanWebpackPlugin(),
+    new IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
   optimization: {
     splitChunks: {
@@ -49,13 +58,7 @@ module.exports = merge(common, {
        * See: https://webpack.js.org/plugins/terser-webpack-plugin
        */
       new TerserPlugin({
-        cache: true,
         parallel: true,
-        terserOptions: {
-          output: {
-            comments: false,
-          },
-        },
       }),
     ],
   },
